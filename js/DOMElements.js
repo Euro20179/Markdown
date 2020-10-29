209ed4835489c53cecbe78943ac796592dc6b0e5
Euro20179 <56898436+Euro20179@users.noreply.github.com>
2020-10-29 15:46:51 -0700

    c-time element

--- js/DOMElements.js
+++ js/DOMElements.js
@@ -256,6 +256,93 @@ class Prompt extends HTMLElement{
     }
 }
 
+class Time extends HTMLElement{
+    constructor(){
+        super()
+        this.unformattedText = this.textContent
+    }
+    updateSelf(f){
+        const date = new Date()
+        this.textContent = this.unformattedText
+        .replaceAll("%a", this.numberToShortDay(date.getDay()))
+        .replaceAll("%A", this.numberToDay(date.getDay()))
+        .replaceAll("%w", date.getDay() + 1)
+        .replaceAll("%d", date.getDate())
+        .replaceAll("%b", this.numberToShortMonth(date.getMonth()))
+        .replaceAll("%B", this.numberToMonth(date.getMonth()))
+        .replaceAll("%m", date.getMonth() + 1)
+        .replaceAll("%y", String(date.getFullYear()).slice(-2))
+        .replaceAll("%Y", date.getFullYear())
+        .replaceAll("%H", date.getHours())
+        .replaceAll("%h", date.getHours() >= 13 ? date.getHours() - 12 : date.getHours())
+        .replaceAll("%p", date.getHours() >= 12 ? "PM" : "AM")
+        .replaceAll("%M", date.getMinutes())
+        .replaceAll("%S", date.getSeconds())
+        .replaceAll("%f", date.getMilliseconds())
+        .replaceAll("%z", date.getTimezoneOffset() / 60)
+        .replaceAll("%X", date.toLocaleTimeString())
+        .replaceAll("%x", date.toLocaleDateString())
+    }
+    connectedCallback(){
+        this.updateSelf()
+    }
+    numberToDay(n){
+        switch(n){
+            case 0: return "Sunday"
+            case 1: return "Monday"
+            case 2: return "Tuesday"
+            case 3: return "Wednesday"
+            case 4: return "Thursday"
+            case 5: return "Friday"
+            case 6: return "Saturday"
+        }
+    }
+    numberToShortDay(n){
+        switch(n){
+            case 0: return "Sun"
+            case 1: return "Mon"
+            case 2: return "Tue"
+            case 3: return "Wed"
+            case 4: return "Thu"
+            case 5: return "Fri"
+            case 6: return "Sat"
+        }
+    }
+    numberToMonth(n){
+        switch (n){
+            case 0: return "January"
+            case 1: return "February"
+            case 2: return "March"
+            case 3: return "April"
+            case 4: return "May"
+            case 5: return "June"
+            case 6: return "July"
+            case 7: return "August"
+            case 8: return "September"
+            case 9: return "October"
+            case 10: return "November"
+            case 11: return "December"
+        }
+    }
+    numberToShortMonth(n){
+        switch(n){
+            case 0: return "Jan"
+            case 1: return "Feb"
+            case 2: return "Mar"
+            case 3: return "Apr"
+            case 4: return "May"
+            case 5: return "Jun"
+            case 6: return "Jul"
+            case 7: return "Aug"
+            case 8: return "Sep"
+            case 9: return "Oct"
+            case 10: return "Nov"
+            case 11: return "Dec"
+        }
+    }
+}
+
+customElements.define("c-time", Time)
 customElements.define('c-upsidedown', Upsidedown)
 customElements.define('c-circled', Circled)
 customElements.define('c-rainbow', Rainbow)
