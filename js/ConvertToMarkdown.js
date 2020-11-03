45dd491cb78980988fa0b6a8b156cab566586065
Euro20179 <56898436+Euro20179@users.noreply.github.com>
2020-11-02 16:03:36 -0800

    font-size: 1rem

--- js/ConvertToMarkdown.js
+++ js/ConvertToMarkdown.js
@@ -108,7 +108,9 @@ const regexes = [
     [
         /(?<!\\)\\(?:DEF(?:INE)?)?EMOJI ?:(.+?): ?(.+?)\\/g,
         (_, name, value) => {
-            userDefinedEmotes[name] = value;
+            if (!(name in userDefinedEmotes)) {
+                userDefinedEmotes[name] = value;
+            }
             return "";
         }
     ],
