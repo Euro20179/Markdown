3fb38cdc6a76e136f0e467fa8e05c228d0c33ea4
Euro20179 <56898436+Euro20179@users.noreply.github.com>
2020-12-01 19:06:01 -0800

    emoji search features

---
+++ Emojis/emoji.js
@@ -0,0 +1,35 @@
+function invisiblitizeElement(elem) {
+    elem.style.display = "none";
+}
+function visibilitizeElement(elem) {
+    elem.style.display = "";
+}
+function setResults(value) {
+    document.getElementById("results-counter").innerHTML = `Results: ${value}`;
+}
+let counter = 0;
+let emojis = document.querySelectorAll(".data-emoji-div");
+let emojiCount = emojis.length;
+const search = document.getElementById("search");
+search.addEventListener("input", e => {
+    new Promise((resolve, reject) => {
+        let value = search.value;
+        for (let elem of emojis) {
+            visibilitizeElement(elem);
+            if (!elem.id.match(value)) {
+                if (!(elem.style.display == "none")) {
+                    invisiblitizeElement(elem);
+                }
+            }
+        }
+        resolve();
+    }).then(() => {
+        for (let elem of emojis) {
+            if (elem.style.display == "none") {
+                counter++;
+            }
+        }
+        setResults(emojiCount - counter);
+    });
+    counter = 0;
+});
